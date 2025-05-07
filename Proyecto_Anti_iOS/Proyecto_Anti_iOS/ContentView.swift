0b818fac507933b3de7337a289fdd7e24b7fc7fb
Leyenddx <aarok_ny224@hotmail.com>
2025-05-07 10:44:07 -0600

    [PRIMER] cambio en el repositorio

---
+++ Proyecto_Anti_iOS/Proyecto_Anti_iOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Proyecto_Anti_iOS
+//
+//  Created by alumno on 5/7/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Proyecto_Anti_iOS/Proyecto_Anti_iOS/Proyecto_Anti_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_Anti_iOSApp.swift
+//  Proyecto_Anti_iOS
+//
+//  Created by alumno on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto_Anti_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
